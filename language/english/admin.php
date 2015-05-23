3e387f0252cde5f9e3f37d4003dffee6bd82220e
mambax7 <mambax7@gmail.com>
2015-05-23 13:38:33 -0400

    Adding English

---
+++ language/english/admin.php
@@ -0,0 +1,24 @@
+<?php
+include_once "../../tadtools/language/{$xoopsConfig['language']}/admin_common.php";
+define("_TAD_NEED_TADTOOLS"," 需要 tadtools 模組，可至<a href='http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50' target='_blank'>Tad教材網</a>下載。");
+
+define("_AND", ",");
+
+//unit.php
+define("_MA_TADREPAIR_UNIT_TITLE", "responsible for maintaining the unit name");
+define("_MA_TADREPAIR_UNIT_ADMIN", "The management personnel");
+define("_MA_TAD_REPAIR_UNIT_FORM", "set the unit responsible for the maintenance and management personnel");
+
+//index.php
+define("_MA_TADREPAIR_REPAIR_SN", "number");
+define("_MA_TADREPAIR_REPAIR_TITLE", "repair contents");
+define("_MA_TADREPAIR_REPAIR_DATE", "repair date");
+define("_MA_TADREPAIR_REPAIR_STATUS", "severity");
+define("_MA_TADREPAIR_REPAIR_STATUS2", "rank");
+define("_MA_TADREPAIR_REPAIR_UID", "repair person");
+define("_MA_TADREPAIR_UNIT", "notice");
+define("_MA_TADREPAIR_FIXED_UID", "respondents");
+define("_MA_TADREPAIR_FIXED_DATE", "Reply Date");
+define("_MA_TADREPAIR_FIXED_STATUS", "processing status");
+define("_MA_TADREPAIR_FIXED_STATUS2", "treatment");
+define("_MA_TADREPAIR_EMPTY", "Congratulations are currently no Bulletin!!");
---
+++ language/english/blocks.php
@@ -0,0 +1,3 @@
+<?php
+
+define("_MB_TADREPAIR_REPAIRED", "has been fixed");
