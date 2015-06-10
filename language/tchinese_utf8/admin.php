b7ec42e7928338e59b867153a4e3852be59957f5
Tad <tad0616@gmail.com>
2015-06-10 14:34:02 +0800

    調整語系

--- language/tchinese_utf8/admin.php
+++ language/tchinese_utf8/admin.php
@@ -1,27 +1,24 @@
 <?php
-include_once "../../tadtools/language/{$xoopsConfig['language']}/admin_common.php";
-define("_TAD_NEED_TADTOOLS"," 需要 tadtools 模組，可至<a href='http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50' target='_blank'>Tad教材網</a>下載。");
+include_once '../../tadtools/language/' . $xoopsConfig['language'] . '/admin_common.php';
+define('_TAD_NEED_TADTOOLS', ' 需要 modules/tadtools，可至<a href="http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50" target="_blank">Tad教材網</a>下載。');
 
-define("_AND","、");
-
+define("_AND", "、");
 
 //unit.php
-define("_MA_TADREPAIR_UNIT_TITLE","負責維護的單位名稱");
-define("_MA_TADREPAIR_UNIT_ADMIN","該單位管理人員");
-define("_MA_TAD_REPAIR_UNIT_FORM","設定負責維護的單位及其管理人員");
+define("_MA_TADREPAIR_UNIT_TITLE", "負責維護的單位名稱");
+define("_MA_TADREPAIR_UNIT_ADMIN", "該單位管理人員");
+define("_MA_TAD_REPAIR_UNIT_FORM", "設定負責維護的單位及其管理人員");
 
 //index.php
-define("_MA_TADREPAIR_REPAIR_SN","編號");
-define("_MA_TADREPAIR_REPAIR_TITLE","報修內容");
-define("_MA_TADREPAIR_REPAIR_DATE","報修日期");
-define("_MA_TADREPAIR_REPAIR_STATUS","嚴重程度");
-define("_MA_TADREPAIR_REPAIR_STATUS2","等級");
-define("_MA_TADREPAIR_REPAIR_UID","報修者");
-define("_MA_TADREPAIR_UNIT","通知");
-define("_MA_TADREPAIR_FIXED_UID","回覆者");
-define("_MA_TADREPAIR_FIXED_DATE","回覆日期");
-define("_MA_TADREPAIR_FIXED_STATUS","處理狀況");
-define("_MA_TADREPAIR_FIXED_STATUS2","處理");
-define("_MA_TADREPAIR_EMPTY","恭喜！目前沒有任何通報！");
-
-?>
+define("_MA_TADREPAIR_REPAIR_SN", "編號");
+define("_MA_TADREPAIR_REPAIR_TITLE", "報修內容");
+define("_MA_TADREPAIR_REPAIR_DATE", "報修日期");
+define("_MA_TADREPAIR_REPAIR_STATUS", "嚴重程度");
+define("_MA_TADREPAIR_REPAIR_STATUS2", "等級");
+define("_MA_TADREPAIR_REPAIR_UID", "報修者");
+define("_MA_TADREPAIR_UNIT", "通知");
+define("_MA_TADREPAIR_FIXED_UID", "回覆者");
+define("_MA_TADREPAIR_FIXED_DATE", "回覆日期");
+define("_MA_TADREPAIR_FIXED_STATUS", "處理狀況");
+define("_MA_TADREPAIR_FIXED_STATUS2", "處理");
+define("_MA_TADREPAIR_EMPTY", "恭喜！目前沒有任何通報！");
