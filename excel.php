79792416db84fdc604845166d32611c65e426567
Tad <tad0616@gmail.com>
2019-04-19 10:36:43 +0800

    修改Excel為xlsx

--- excel.php
+++ excel.php
@@ -96,10 +96,10 @@ $objActSheet->mergeCells("A{$i}:K{$i}")->setCellValue("A{$i}", '=CONCATENATE("' 
 $title = $ym . _MD_TADREPAIR_REPORT;
 $title = (_CHARSET === 'UTF-8') ? iconv('UTF-8', 'Big5', $title) : $title;
 header('Content-Type: application/vnd.ms-excel');
-header("Content-Disposition: attachment;filename={$title}.xls");
+header("Content-Disposition: attachment;filename={$title}.xlsx");
 header('Cache-Control: max-age=0');
 
-$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
 $objWriter->setPreCalculateFormulas(false);
 $objWriter->save('php://output');
 exit;
